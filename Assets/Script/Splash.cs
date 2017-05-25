262ef7c52fc7804ebca81f0d110107470e1a14b1
diasantoso <mikhaelmds@gmail.com>
2017-05-25 11:40:54 +0700

    Add Menu & Splash

---
+++ Assets/Script/Splash.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splash : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("changescene", 3f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+  
+    void changescene()
+    {
+        
+        Application.LoadLevel("menu");
+
+    }
+
+}
